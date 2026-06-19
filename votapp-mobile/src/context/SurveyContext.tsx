c16e5e28bfdee9ac3bb9e9fe0a6db3169cd470a6
Sidney <grupo.lockhart@gmail.com>
2026-06-19 09:42:49 -0400

    Commit 06 19 26

--- votapp-mobile/src/context/SurveyContext.tsx
+++ votapp-mobile/src/context/SurveyContext.tsx
@@ -1,3 +1,6 @@
+
+//votapp-mobile/src/context/surveyContext.tsx
+
 import React, { createContext, useContext, useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { API_URL } from "../config/api";
